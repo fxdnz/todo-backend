f314310720ece3a3400ad73a9db584912b692208
Steaven <mirandsteavenjohann@gmail.com>
2025-04-09 21:14:15 +0800

    gana na

--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ python manage.py migrate
 
 # Check if CREATE_SUPERUSER is set to 'true' and create superuser
 if [[$CREATE_SUPERUSER]]; then
-    python manage.py createsuperuser --no-input
+    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
