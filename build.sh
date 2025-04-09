ff5075ca3f5991508a6c1f6e2e61baffba28d536
Steaven <mirandsteavenjohann@gmail.com>
2025-04-09 20:46:07 +0800

    get credential

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
-set -o errexit
+set -o errexit  # Exit on error
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
 
+# Run migrations
 python manage.py migrate
 
-if [["$CREATE_SUPERUSER" == "true"]];
-then 
-    python manage.py createsuperuser --no-input
-fi  
+# Check if CREATE_SUPERUSER is set to 'true' and create superuser
+if [[ "$CREATE_SUPERUSER" == "true" ]]; then
+    python manage.py createsuperuser --no-input \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL" \
+        --password "$DJANGO_SUPERUSER_PASSWORD"
+fi
